4e0d09d934ace528d0501f1e2eecf81ef03d4f7c
20shiy <shiying6120@gmail.com>
2022-06-09 21:21:56 -0400

    finished lab 1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profile_image from "../assets/LinShiying.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src = {profile_image}
+          alt="Profile Pic"
+          ></img>
       </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Shiying Lin</div>
+        <div className="brief_description">
+          Hi! This is Shiying
+        </div>
+      </div>
+    </div>
+  </div>
+
     )
   }
 }
